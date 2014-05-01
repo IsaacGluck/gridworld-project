2f9b81514082d64b1d7f5a5686fe9de4f554f61d
Alicia Chen <mail4aliciac@gmail.com>
2014-05-01 16:56:59 -0400

    QuickCritter needs a fixin

---
+++ QuickCrab.java
@@ -0,0 +1,64 @@
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+/*  A QuickCrab moves to one of the two locations, randomly selected, that are two spaces to its right or left, if that location and the intervening location are both empty. Otherwise, a QuickCrab moves like a CrabCritter.*/
+
+public class QuickCrab extends CrabCritter{
+    /*
+     * Finds the valid adjacent locations 2 cells over of this critter in different directions.
+     * @param directions - an array of directions (which are relative to the
+     * current direction)
+     * @return a set of valid locations that are two spaces of the current
+     * location in the given directions (left and right)
+     */
+
+    public ArrayList<Location> getLocationsInDirections2(int[] directions){
+	ArrayList <Location> locs = new ArrayList<Location>();
+	Grid gr = getGrid();
+	Location loc = getLocation();
+	
+	for (int d: directions)
+	    {
+		Location neighborLoc = loc.getAdjacentLocation(getAdjacentLocation(getDirection() + d));
+		if (gr.isValid(neighborLoc))
+		    locs.add(neighborLoc);
+	    }
+	return locs;
+    }
+
+    public ArrayList<Location> getMoveLocations2(){
+	ArrayList<Location> locs = new ArrayList<Location>();
+	int[] dirs = { Location.LEFT, Location.RIGHT };
+	for (Location loc : getLocationsInDirections2(dirs))
+	    if (getGrid().get(loc) == null)
+		locs.add(loc);
+	return locs;
+    }
+
+  
+    public void act(){
+	if (getMoveLocations2().size() == 0){
+	    super.act();
+		}
+	else{
+	    if (getGrid() == null)
+		return;
+	    ArrayList<Actor> actors = getActors();
+	    processActors(actors);
+	    ArrayList<Location> moveLocs = getMoveLocations2();
+	    Location loc = selectMoveLocation(moveLocs);
+	    makeMove(loc);
+	}
+    }
+}
+
+
+	
+		
+		
+    
